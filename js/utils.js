b0ae36bc6ec12d763962f381b0fc64af9c99cfc3
theothernamesweretaken <donotreplyatalltome@gmail.com>
2022-04-23 09:35:48 -0700

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -487,11 +487,11 @@ const theme_names = {
 function changeTheme() {
 	let aqua = player.theme == "aqua"
 	colors_theme = colors[player.theme || "default"]
-	document.body.style.setProperty('--background', aqua ? "#001f3f" : "#0f0f0f")
-	document.body.style.setProperty('--background_tooltip', aqua ? "rgba(0, 15, 31, 0.75)" : "rgba(0, 0, 0, 0.75)")
-	document.body.style.setProperty('--color', aqua ? "#bfdfff" : "#dfdfdf")
-	document.body.style.setProperty('--points', aqua ? "#dfefff" : "#ffffff")
-	document.body.style.setProperty("--locked", aqua ? "#c4a7b3" : "#bf8f8f")
+	document.body.style.setProperty('--background', aqua ? "#001f3f" : "#f0f0f0")
+	document.body.style.setProperty('--background_tooltip', aqua ? "rgba(0, 15, 31, 0.75)" : "rgba(240, 240, 240, 0.75)")
+	document.body.style.setProperty('--color', aqua ? "#bfdfff" : "#fdfdfd")
+	document.body.style.setProperty('--points', aqua ? "#dfefff" : "#000000")
+	document.body.style.setProperty("--locked", aqua ? "#c4a7b3" : "#fb8f8f")
 }
 
 function getThemeName() {
